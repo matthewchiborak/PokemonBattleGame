9bbdceefaed9bc9763b37ea1e5f31a21780e1189
Jugal Patel <jpate43@uwo.ca>
2015-12-10 15:56:02 -0500

    Added status draw.

--- PokemonBattleGame/BattleScreen.h
+++ PokemonBattleGame/BattleScreen.h
@@ -52,6 +52,7 @@ public:
 	void SetOPHealth(int health);//set the HP of the opponet
 	void SetSelfHealth(int health);//set the health of the user's current pokemone
 	void refreshHealth();
+	void refreshStatus();
 	void keysPressed(std::vector<sf::Keyboard::Key> key);//respond to pressed keys
 	void setOppPokemon(Pokemon * p);
 	void setSelfPokemon(Pokemon * p);
